122a14771be1039aad8f24cc2d359cc14260daa9
Sebastian Ewak <sebastian.ewak@gmail.com>
2017-09-10 15:51:28 +0200

    First move - test #1 - has 5 disks on board

--- ReversiTDD/ReversiTDDTests/ReversiSpec.swift
+++ ReversiTDD/ReversiTDDTests/ReversiSpec.swift
@@ -2,6 +2,10 @@
 //  ReversiSpec.swift
 //  ReversiTDD
 //
+//  Based on:
+//  1. https://en.wikipedia.org/wiki/Reversi
+//  2. http://www.hannu.se/games/othello/rules.htm
+//
 //  Created by Krzysztof Moczała on 03/09/2017.
 //  Copyright © 2017 ArcyCoders. All rights reserved.
 //
@@ -25,13 +29,29 @@ struct Field: Equatable {
     }
 }
 
-struct Board: Equatable {
-    let taken: [Field]
+class Board: Equatable {
+    enum ApplyMoveError: Error {
+        case fieldAlreadyTaken
+    }
+
+    fileprivate var taken: [Field]
+
+    var numberOfFieldsTaken: Int {
+        return taken.count
+    }
     
     init(taken: [Field]) {
         self.taken = taken
     }
-    
+
+    func move(to targetField: Field) throws {
+        if taken.contains(where: { $0.x == targetField.x && $0.y == targetField.y }) {
+            throw ApplyMoveError.fieldAlreadyTaken
+        }
+
+        taken.append(targetField)
+    }
+
     static func == (lhs: Board, rhs: Board) -> Bool {
         return lhs.taken == rhs.taken
     }
@@ -43,10 +63,16 @@ func == (tuple1:(Int, Int, Disk),tuple2:(Int, Int, Disk)) -> Bool
 }
 
 class Reversi {
-    var board: Board
+    var numberOfFieldsTaken: Int { return board.numberOfFieldsTaken }
+
+    fileprivate(set) var board: Board
     
     init() {
         board = Board(taken: [Field(x: 3, y: 3, disk: .White), Field(x: 3, y: 4, disk: .Black), Field(x: 4, y: 3, disk: .Black), Field(x: 4, y: 4, disk: .White)])
+    }
+
+    func move(to targetField: Field) throws {
+        try board.move(to: targetField)
     }
 }
 
@@ -55,17 +81,26 @@ class ReversiSpec: QuickSpec {
         
         describe("start") {
             
-            it("has 4 disk in a starting possition") {
+            it("has 4 disk in a starting position") {
                 let game = Reversi()
                 
                 expect(game.board).to(equal(Board(taken: [Field(x: 3, y: 3, disk: .White), Field(x: 3, y: 4, disk: .Black), Field(x: 4, y: 3, disk: .Black), Field(x: 4, y: 4, disk: .White)])))
             }
             
         }
-        
+
+        // Rules:
+        // 1. performed by dark
+        // 2. must c4, d3, e6, f5
+        // 3. must flip white disk between dark ones
         describe("first move") {
-            // HOMEWORK
+
+            it("has total of 5 disks on board") {
+                let game = Reversi()
+                try? game.move(to: Field(x: 2, y: 3, disk: .Black))
+
+                expect(game.numberOfFieldsTaken).to(equal(5))
+            }
         }
-        
     }
 }
