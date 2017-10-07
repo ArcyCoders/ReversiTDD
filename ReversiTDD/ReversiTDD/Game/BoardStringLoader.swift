7af189bc5ef8bc4f7b19032a8814fe6cf5f79401
Sebastian Ewak <sebastian.ewak@gmail.com>
2017-10-07 18:54:22 +0200

    Add first unit test for FlankedFieldsFinderSpec

---
+++ ReversiTDD/ReversiTDD/Game/BoardStringLoader.swift
@@ -0,0 +1,44 @@
+//
+//  BoardStringLoader.swift
+//  ReversiTDD
+//
+//  Created by Sebastian Ewak on 07/10/2017.
+//  Copyright © 2017 ArcyCoders. All rights reserved.
+//
+
+import Foundation
+
+public class BoardStringLoader {
+    public init() {}
+
+    public func load(from rows: String...) -> Board {
+        let defaultBoard = Board()
+        let rows = rows.filter { $0.characters.count == 8 }
+
+        guard rows.count == 8 else { return defaultBoard }
+
+        let resultBoard = Board()
+        for rowIndex in 0..<8 {
+            for columnIndex in 0..<8 {
+                let rowString = rows[rowIndex]
+                let disk: Disk? = getField(forCharacter: Array(rowString)[columnIndex])
+
+                if let disk = disk,
+                   let column = Column(rawValue: columnIndex),
+                   let row = Row(rawValue: rowIndex) {
+                    resultBoard.add(field: Field(column: column, row: row, disk: disk))
+                }
+            }
+        }
+
+        return resultBoard
+    }
+
+    private func getField(forCharacter fieldCharacter: Character) -> Disk? {
+        switch fieldCharacter {
+            case "W": return Disk(currentColor: .white)
+            case "B": return Disk(currentColor: .black)
+            default: return nil
+        }
+    }
+}
--- ReversiTDD/ReversiTDDTests/FlankedFieldsFinderSpec.swift
+++ ReversiTDD/ReversiTDDTests/FlankedFieldsFinderSpec.swift
@@ -13,6 +13,32 @@ import ReversiTDD
 
 class FlankedFieldsFinderSpec: QuickSpec {
     override func spec() {
-        var flankedFieldsFinder = FlankedFieldsFinder()
+        var flankedFieldsFinder: FlankedFieldsFinder!
+        var board: Board!
+        var flankedFields: [Field]!
+        var targetField: Field!
+
+        beforeEach {
+            flankedFieldsFinder = FlankedFieldsFinder()
+            flankedFields = []
+        }
+
+        describe("getAllFlankedFields") {
+            it("returns single item when only one white piece on between target and other black when checking right") {
+                targetField = Field(column: .a, row: ._1, disk: Disk(currentColor: .black))
+                board = BoardStringLoader().load(from: "BWB-----",
+                                                       "--------",
+                                                       "--------",
+                                                       "--------",
+                                                       "--------",
+                                                       "--------",
+                                                       "--------",
+                                                       "--------"
+                )
+                flankedFields = flankedFieldsFinder.getAllFlankedFields(byTargetField: targetField, onBoard: board)
+
+                expect(flankedFields.count).to(equal(1))
+            }
+        }
     }
 }
