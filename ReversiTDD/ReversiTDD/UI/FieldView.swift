6e8002ab150eef26f2ce3e7c7cfec9af92711ba3
Sebastian Ewak <sebastian.ewak@gmail.com>
2017-10-14 17:12:30 +0200

    Add field view for UI

---
+++ ReversiTDD/ReversiTDD/UI/FieldView.swift
@@ -0,0 +1,65 @@
+//
+//  FieldView.swift
+//  ReversiTDD
+//
+//  Created by Sebastian Ewak on 14/10/2017.
+//  Copyright © 2017 ArcyCoders. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+@IBDesignable
+class FieldView: UIView {
+    private weak var diskLayer: CALayer!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        commonInit()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let diskFrame = bounds.insetBy(dx: 6, dy: 6)
+        diskLayer.frame = diskFrame
+        diskLayer.cornerRadius = 0.5 * diskFrame.height
+    }
+
+    func setup(withField field: Field) {
+        if let disk = field.disk {
+            diskLayer.backgroundColor = disk.currentColor == .white ? UIColor.white.cgColor : UIColor.black.cgColor
+        }
+        else {
+            diskLayer.backgroundColor = UIColor.clear.cgColor
+        }
+
+        layer.removeAllAnimations()
+    }
+
+    func markAsValidField() {
+        UIView.animate(withDuration: 1.0, delay: 0, options: [.repeat, .autoreverse], animations: { [weak self] in
+            self?.backgroundColor = .green
+            self?.backgroundColor = .clear
+            }, completion: nil)
+    }
+
+    private func commonInit() {
+        backgroundColor = UIColor.lightGray
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 2
+
+        let diskLayer = CALayer()
+        diskLayer.frame = frame
+        diskLayer.backgroundColor = UIColor.clear.cgColor
+        self.diskLayer = diskLayer
+        layer.addSublayer(diskLayer)
+    }
+}
